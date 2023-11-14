70bda96c80b1e3a903d9998489f5192968610df0
Brandon Montijo <bcm8504@rit.edu>
2023-11-14 11:33:58 -0500

    Add rendering tools

--- src/renderer/renderStatus.tsx
+++ src/renderer/renderStatus.tsx
@@ -1,31 +1,31 @@
-import * as React from 'react';
-import ReportProblemIcon from '@mui/icons-material/ReportProblem';
-import InfoIcon from '@mui/icons-material/Info';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
-import DoneIcon from '@mui/icons-material/Done';
-import Chip from '@mui/material/Chip';
-import { styled } from '@mui/material/styles';
-import {GridRenderCellParams} from "@mui/x-data-grid";
-import {ReactNode} from "react";
+import * as React from "react";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import InfoIcon from "@mui/icons-material/Info";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import DoneIcon from "@mui/icons-material/Done";
+import Chip from "@mui/material/Chip";
+import { styled } from "@mui/material/styles";
+import { GridRenderCellParams } from "@mui/x-data-grid";
+import { ReactNode } from "react";
 
 const StyledChip = styled(Chip)(({ theme }) => ({
-    justifyContent: 'left',
-    '& .icon': {
-        color: 'inherit',
+    justifyContent: "left",
+    "& .icon": {
+        color: "inherit",
     },
-    '&.New': {
+    "&.New": {
         color: theme.palette.info.dark,
         border: `1px solid ${theme.palette.info.main}`,
     },
-    '&.Completed': {
+    "&.Approved": {
         color: theme.palette.success.dark,
         border: `1px solid ${theme.palette.success.main}`,
     },
-    '&.InProgress': {
+    "&.InProgress": {
         color: theme.palette.warning.dark,
         border: `1px solid ${theme.palette.warning.main}`,
     },
-    '&.Rejected': {
+    "&.Rejected": {
         color: theme.palette.error.dark,
         border: `1px solid ${theme.palette.error.main}`,
     },
@@ -39,32 +39,38 @@ const Status = React.memo((props: StatusProps) => {
     const { status } = props;
 
     let icon: any = null;
-    if (status === 'Rejected') {
-        icon = <ReportProblemIcon className="icon" />;
-    } else if (status === 'New') {
-        icon = <InfoIcon className="icon" />;
-    } else if (status === 'In Progress') {
-        icon = <AutorenewIcon className="icon" />;
-    } else if (status === 'Completed') {
-        icon = <DoneIcon className="icon" />;
+    if (status === "Rejected") {
+        icon = <ReportProblemIcon className='icon' />;
+    } else if (status === "New") {
+        icon = <InfoIcon className='icon' />;
+    } else if (status === "In Progress") {
+        icon = <AutorenewIcon className='icon' />;
+    } else if (status === "Approved") {
+        icon = <DoneIcon className='icon' />;
     }
 
     let label: string = status;
-    if (status === 'PartiallyFilled') {
-        label = 'Partially Filled';
+    if (status === "PartiallyFilled") {
+        label = "Partially Filled";
     }
 
     return (
-        <StyledChip className={status.replace(' ', '')} icon={icon} size="small" label={label} variant="outlined" />
+        <StyledChip
+            className={status.replace(" ", "")}
+            icon={icon}
+            size='small'
+            label={label}
+            variant='outlined'
+        />
     );
 });
 
 const renderStatus = (params: GridRenderCellParams): ReactNode => {
     if (params.value == null) {
-        return '';
+        return "";
     }
 
     return <Status status={params.value} />;
-}
+};
 
 export default renderStatus;
