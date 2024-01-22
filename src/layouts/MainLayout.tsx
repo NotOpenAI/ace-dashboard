a0668cc1cd8f0e9bac57af5424494eea741d46ac
Brandon Montijo <bcm8504@rit.edu>
2024-01-22 12:20:41 -0500

    Update MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,29 +1,259 @@
-import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
-import {Outlet} from "react-router-dom";
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DataThresholdingIcon from '@mui/icons-material/DataThresholding';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import PeopleIcon from '@mui/icons-material/People';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Link } from '@mui/material';
+import { useEffect } from 'react';
+import { capitalizeEachWord } from '../utils/capitalizeEachWord.tsx';
 
-const navItems = ['bids', 'customers', 'users', 'operational data', 'login']
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+}));
+
+const navItems = ['bids', 'customers', 'operational data', 'users'];
 
 const MainLayout = () => {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const location = useLocation();
+    const [activePage, setActivePage] = React.useState('');
+
+    useEffect(() => {
+        const pathname = location.pathname.substring(1).replace('-', ' ');
+        setActivePage(pathname);
+    }, [location.pathname]);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Box sx={{display: 'flex', width: '100%'}}>
-            <AppBar>
-                <Toolbar variant={'dense'}>
-                    <Typography variant={'h6'} color={'inherit'} sx={{
-                        flexGrow: 1, display: {
-                            xs: 'none', sm: 'block'
-                        }
-                    }}>Dashboard</Typography>
-                    <Box sx={{display: {xs: 'none', sm: 'block'}}}>
-                            {navItems.map((item) => (
-                                <Button key={item} color={'inherit'} href={item.replace(' ', '')}>
-                                    {item}
-                                </Button>
-                            ))}
-                    </Box>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position='fixed' open={open}>
+                <Toolbar>
+                    <IconButton
+                        color='inherit'
+                        aria-label='open drawer'
+                        onClick={handleDrawerOpen}
+                        edge='start'
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant='h6' noWrap component='div'>
+                        Ace Dashboard
+                    </Typography>
                 </Toolbar>
             </AppBar>
-            <Box sx={{display: 'flex', width: '100%', marginTop: 7}}>
-                <Outlet/>
+            <Drawer variant='permanent' open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? (
+                            <ChevronRightIcon />
+                        ) : (
+                            <ChevronLeftIcon />
+                        )}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {navItems.map((text) => (
+                        <Link
+                            href={text.replace(' ', '-')}
+                            sx={{
+                                textDecoration: 'none',
+                                color: 'inherit',
+                            }}
+                        >
+                            <ListItem
+                                key={text}
+                                disablePadding
+                                sx={{ display: 'block' }}
+                            >
+                                <ListItemButton
+                                    sx={{
+                                        minHeight: 48,
+                                        justifyContent: open
+                                            ? 'initial'
+                                            : 'center',
+                                        px: 2.5,
+                                        borderRadius: 4,
+                                        m: 1,
+                                        backgroundColor:
+                                            activePage === text.toLowerCase()
+                                                ? theme.palette.action.selected
+                                                : 'transparent',
+                                    }}
+                                >
+                                    <ListItemIcon
+                                        sx={{
+                                            minWidth: 0,
+                                            mr: open ? 3 : 'auto',
+                                            justifyContent: 'center',
+                                        }}
+                                    >
+                                        {text === 'bids' && <ViewListIcon />}
+                                        {text === 'customers' && <PeopleIcon />}
+                                        {text === 'users' && (
+                                            <ManageAccountsIcon />
+                                        )}
+                                        {text === 'operational data' && (
+                                            <DataThresholdingIcon />
+                                        )}
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={capitalizeEachWord(text)}
+                                        sx={{ opacity: open ? 1 : 0 }}
+                                    />
+                                </ListItemButton>
+                            </ListItem>
+                        </Link>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    <Link
+                        href={'account'}
+                        sx={{
+                            textDecoration: 'none',
+                            color: 'inherit',
+                        }}
+                    >
+                        <ListItem
+                            key={'account'}
+                            disablePadding
+                            sx={{ display: 'block' }}
+                        >
+                            <ListItemButton
+                                sx={{
+                                    minHeight: 48,
+                                    justifyContent: open ? 'initial' : 'center',
+                                    px: 2.5,
+                                    backgroundColor:
+                                        activePage === 'account'
+                                            ? theme.palette.action.selected
+                                            : 'transparent',
+                                }}
+                            >
+                                <ListItemIcon
+                                    sx={{
+                                        minWidth: 0,
+                                        mr: open ? 3 : 'auto',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <AccountCircleIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'Account'}
+                                    sx={{ opacity: open ? 1 : 0 }}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                </List>
+            </Drawer>
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, marginTop: 10, width: '88%' }}
+            >
+                <Outlet />
             </Box>
         </Box>
     );
