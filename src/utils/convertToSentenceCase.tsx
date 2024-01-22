62d3cc87c1a9f986556f87238f8d87aa03682139
Brandon Montijo <bcm8504@rit.edu>
2024-01-22 10:29:22 -0500

    Create convertToSentenceCase.tsx

---
+++ src/utils/convertToSentenceCase.tsx
@@ -0,0 +1,13 @@
+const convertToSentenceCase = (inputString: string) => {
+    const words = inputString.split('_').map((word) => word.toLowerCase());
+
+    const capitalizedWords = words.map(
+        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+
+    const sentenceCaseString = capitalizedWords.join(' ');
+
+    return sentenceCaseString;
+};
+
+export default convertToSentenceCase;
