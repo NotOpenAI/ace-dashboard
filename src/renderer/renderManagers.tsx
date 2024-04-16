92980f52bfae1096375f88bda35fb775e862f37b
Brandon Montijo <bcm8504@rit.edu>
2024-04-16 10:50:00 -0400

    Fix renderManagers format bug

--- src/renderer/renderManagers.tsx
+++ src/renderer/renderManagers.tsx
@@ -1,7 +1,7 @@
+import { Button, ButtonGroup, Stack } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { NavLink as RouterLink } from 'react-router-dom';
 import { GridRenderCellParams } from '@mui/x-data-grid';
-import { Button, Stack } from '@mui/material';
 import { Manager } from '../types/Role.tsx';
 import { ReactNode } from 'react';
 
@@ -11,24 +11,26 @@ const renderManagers = (params: GridRenderCellParams): ReactNode => {
             direction={'row'}
             justifyContent={'space-between'}
             alignItems={'center'}
-            spacing={2}
+            spacing={1}
             sx={{ overflowX: 'auto', whiteSpace: 'nowrap' }}
         >
-            {params.value.map((manager: Manager) => (
-                <Button
-                    key={manager.id}
-                    size={'small'}
-                    startIcon={<OpenInNewIcon />}
-                    color={'inherit'}
-                    variant={'outlined'}
-                    sx={{ textTransform: 'none' }}
-                    component={RouterLink}
-                    to={`/users/${manager.id}`}
-                    target={'_blank'}
-                >
-                    {`${manager.first_name} ${manager.last_name}`}
-                </Button>
-            ))}
+            <ButtonGroup>
+                {params.value.map((manager: Manager) => (
+                    <Button
+                        key={manager.id}
+                        size={'small'}
+                        startIcon={<OpenInNewIcon />}
+                        color={'inherit'}
+                        variant={'outlined'}
+                        sx={{ textTransform: 'none' }}
+                        component={RouterLink}
+                        to={`/users/${manager.id}`}
+                        target={'_blank'}
+                    >
+                        {`${manager.first_name} ${manager.last_name}`}
+                    </Button>
+                ))}
+            </ButtonGroup>
         </Stack>
     );
 };
