ad464c0820a2eb6c44c3956181c8b98c79b36e58
Brandon Montijo <bcm8504@rit.edu>
2024-04-23 09:08:01 -0400

    Create RouteLink component
    Realized that the only router working at a production level (ie. `yarn build`) was NavLink from react-router-dom. Will switch all links to use this

---
+++ src/components/RouteLink.tsx
@@ -0,0 +1,25 @@
+import { Typography } from '@mui/material';
+import { NavLink as RouterLink } from 'react-router-dom';
+
+type RouteLinkProps = {
+    to: string;
+    label: string;
+};
+export const RouteLink = ({ to, label }: RouteLinkProps) => {
+    return (
+        <Typography
+            variant={'body1'}
+            sx={{
+                color: 'inherit',
+                textDecoration: 'none',
+                '&:hover': {
+                    textDecoration: 'underline',
+                },
+            }}
+            component={RouterLink}
+            to={to}
+        >
+            {label}
+        </Typography>
+    );
+};
